c07e3a21970137927beb0507e7304ef7ec5d5447
Wuwen YAO <wuwen123@login1.int.fir.alliancecan.ca>
2025-08-12 16:25:04 -0700

    for clusters

--- wuwen.sh
+++ wuwen.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 export OMP_NUM_THREADS=80
 echo "Running LAMMPS bicrystal simulation..."
-# --- 关键修改：在这里指定 LAMMPS 可执行文件的完整路径 ---
+
 /home/wuwen/lammps/build/lmp -in in.impact
 
 if [ -f traj.lammpstrj ]; then
